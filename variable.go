66e0ba106ca3ada7c2d669ac147356ce960cc6a7
MD. AL AMIN - AL - SHAHARIA <66860837+al-amin-alshaharia@users.noreply.github.com>
2025-06-23 22:30:07 +0600

    Initial commit

---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 2
+	fmt.Println("The value of a is:", a)
+	var b int = 10
+	fmt.Println("The value of b is:", b)
+	c := 209
+	fmt.Println(c)
+}
